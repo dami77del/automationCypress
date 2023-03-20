6370d19b64fee80ec44811a96916ac7317e655b5
dami77del <ddeligdisch@gmail.com>
2023-03-20 23:25:45 +0100

    add new test2

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -10,20 +10,25 @@ describe('my first test Suite', () => {
         cy.wait(2000)
         cy.get('.product').should('have.length', 5)
         cy.get('.product:visible').should('have.length', 4)
-        cy.get('.products').find('.product').should('have.length', 4)
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
 
-        cy.get('.products').find('.product').each(($el, index, $list) => {
+        cy.get ('.products').as('productLocator') /// change name for .products to 'productLocator
+        cy.get('@productLocator').find('.product').should('have.length', 4)  // hier can see the change into de metod GET
+        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
+
+        cy.get('@productLocator').find('.product').each(($el, index, $list) => {
             const textVeg3 = $el.find('h4.product-name').text()
             if (textVeg3.includes('Cashews')) {
                 cy.wrap($el).find('button').click()
             }
         }
         )
+        //assert if logo text is correctly displayed
+        cy.get('.brand').should('have.text','GREENKART')
 
         cy.get('.brand').then((logo) => {
             cy.log(logo.text())
             cy.log(logo.contents('GREENKART'))
+            console.log('this name is ok'  )
         })
     });
 })
