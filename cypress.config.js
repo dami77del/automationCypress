2d7044dabe4796d1d53b168b162e48ca5f95d3ab
dami77del <ddeligdisch@gmail.com>
2023-04-02 00:16:54 +0200

    add ecommerce featuche integration + modules +package json

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
 
 module.exports = defineConfig({
 
   defaultCommandTimeout: 4200,
-
+  reporter:'mochawesome',
   env: {
     URL: "https://www.rahulshettyacademy.com/"
   },
@@ -14,9 +27,7 @@ module.exports = defineConfig({
   projectId: "q7hxaf",
   CYPRESS_RECORD_KEY:'4da8e755-c335-488c-94a9-b811e0937469',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    specPattern: 'cypress/integration/examples/*.js'
+    setupNodeEvents,
+    specPattern: 'cypress/integration/examples/BDD/*.feature',
   },
 });
--- cypress/integration/examples/Test8Framework.js
+++ cypress/integration/examples/Test8Framework.js
@@ -32,21 +32,7 @@ describe('my firt test Framework', () => {
         });
         
         homePage.getCartButton().click()
-        var sum = 0
-        cy.get('tr td:nth-child(4) strong').each((el, index, list) => {
-            const amount = el.text()
-            var res = amount.split(" ")
-            res = res[1].trim()
-            sum = Number(sum) + Number(res)
-        }).then(() => {
-            cy.log('Total Price:', sum)
-        })
-        cy.get('h3 strong').then((element) => {
-            const amount = element.text()
-            var res = amount.split(" ")
-            var total = res[1].trim()
-            expect(Number(total)).to.equal(sum)
-        })
+       
         productPage.getCheckout().click()
         chekOutPage.getCountry().type('India')
         cy.wait(2000)
